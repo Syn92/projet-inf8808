c1d8cc600ae0e796e027f6975c582d0c691ce4dc
Jeremy Boulet <jeremyboulet85@gmail.com>
2021-04-09 13:46:49 -0400

    Added d3 plus test graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { DataService } from './data-service.service';
+import { TestD3Component } from './test-d3/test-d3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestD3Component
   ],
   imports: [
     BrowserModule,
