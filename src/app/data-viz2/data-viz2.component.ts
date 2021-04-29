5fde8dd20cd582d71ee7f3c4ed1025e6635734aa
thien-ng <duc-thien@sympatico.ca>
2021-04-29 16:46:32 -0400

    vertical line and circle

--- src/app/data-viz2/data-viz2.component.ts
+++ src/app/data-viz2/data-viz2.component.ts
@@ -12,10 +12,15 @@ export class DataViz2Component implements OnInit {
 
   showLog = true
 
-
+  colors;
+  
   btcPrices = [];
   sp500 = []
   cherryPickDates = []
+  data = [
+    {type:'btc', values: []}, 
+    {type:'sp', values: []}
+  ]
 
 
   constructor(private dataService: DataService, private http: HttpClient) {
@@ -31,6 +36,7 @@ export class DataViz2Component implements OnInit {
     }).finally(async () => {
       this.btcPrices = await this.dataService.getProcessedDataViz2();
       this.cherryPickDate(this.btcPrices, this.sp500)
+      
       this.displayGraph();
     });
   }
@@ -44,6 +50,9 @@ export class DataViz2Component implements OnInit {
             sp_price: spElement["S&P 500"],
             btc_price: btcElement[1]
           })
+          const date = d3.timeParse("%Y-%m-%d")(spElement["Effective date"])          
+          this.data[0]['values'].push([date, btcElement[1]])
+          this.data[1]['values'].push([date, spElement["S&P 500"]])
         }
       });
     });
@@ -68,12 +77,17 @@ export class DataViz2Component implements OnInit {
             "translate(" + margin.left + "," + margin.top + ")");
 
     // Add X axis --> it is a date format
-    var x = d3.scaleTime()
-      .domain(d3.extent(comp.cherryPickDates, function(d) { return d3.timeParse("%Y-%m-%d")(d.date); }))
+    const date: Date[] = []
+    this.data[0].values.forEach(e => {
+      date.push(e[0])
+    })
+    
+    var xScale = d3.scaleTime()
+      .domain(d3.extent(date))
       .range([ 0, width ]);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(xScale));
 
     // Add Y axis
     var y
@@ -94,89 +108,126 @@ export class DataViz2Component implements OnInit {
           return `${comp.nFormatter(y, 0)}`
         }))
       
+      console.log(comp.cherryPickDates)
     // Add the line
-    svg.append("path")
-      .datum(comp.cherryPickDates)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .curve(d3.curveBasis)
-        .x(function(d: any) { return x(d3.timeParse("%Y-%m-%d")(d.date)) })
-        .y(function(d: any) { return y(d.btc_price) })
-        )
-    svg.append("path")
-      .datum(comp.cherryPickDates)
-      .attr("fill", "none")
-      .attr("stroke", "red")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .curve(d3.curveBasis)
-        .x(function(d: any) { return x(d3.timeParse("%Y-%m-%d")(d.date)) })
-        .y(function(d: any) { return y(d.sp_price) })
-        )
 
-    var lines = document.getElementsByClassName('line');
+    comp.colors = d3.scaleOrdinal()
+      .domain(this.data.map(d => d.type))
+      .range(['#4862B4', '#A6D3A6'])
 
-    svg.append("rect")
-      .attr('width', width) // can't catch mouse events on a g element
+    var line = d3.line()
+      .x(d => xScale(d[0]))
+      .y(d => y(d[1]))
+
+    var lineGroup = svg.append('g')
+      .attr('class', 'lines2')
+      .selectAll('.line-group2')
+      .data(this.data).enter()
+      .append('g')
+      .attr('class', 'line')
+
+    const path = lineGroup.append('path')
+      .attr('id', d => d.type)
+      .attr('d', d => line(d.values))
+      .style('stroke', d => comp.colors(d.type))
+      .style('stroke-weight', 1.5)
+      .style('fill', 'none')
+
+    
+     var mouseGroup = svg.append('g')
+       .attr('class', 'mouse-over-effects2')
+  
+     mouseGroup.append('path')
+       .attr('class', 'mouse-line2')
+       .style('stroke', '#A9A9A9')
+       .style("stroke-width", 2)
+       .style("opacity", "0");
+  
+     var lines = document.getElementsByClassName('line') as any;
+
+     var mousePerLine = mouseGroup.selectAll('.mouse-per-line2')
+       .data(this.data)
+       .enter()
+       .append("g")
+       .attr("class", "mouse-per-line2");
+
+    mousePerLine.append("circle")
+      .attr("r", 5)
+      .style("stroke", function(d) {
+        return comp.colors(d.type);
+      })
+      .style("fill", "none")
+      .style("stroke-width", "2px")
+      .style("opacity", "0");
+
+    mousePerLine.append("text")
+      .attr("transform", "translate(10,3)")
+      .text("TEST")
+
+    mouseGroup.append('svg:rect')
+      .attr('width', width) 
       .attr('height', height)
       .attr('fill', 'none')
       .attr('pointer-events', 'all')
-      .on('mouseout', function() { // on mouse out hide line, circles and text
-        d3.select(".mouse-line")
+      .on('mouseout', function () { // on mouse out hide line, circles and text
+         d3.select(".mouse-line2")
+           .style("opacity", "0");
+         d3.selectAll(".mouse-per-line2 circle")
           .style("opacity", "0");
-        d3.selectAll(".mouse-per-line circle")
+        d3.selectAll(".mouse-per-line2 text")
           .style("opacity", "0");
-        d3.selectAll(".mouse-per-line text")
-          .style("opacity", "0");
+
       })
-      .on('mouseover', function() { // on mouse in show line, circles and text
-        d3.select(".mouse-line")
+      .on('mouseover', function () { // on mouse in show line, circles and text
+        d3.select(".mouse-line2")
           .style("opacity", "1");
-        d3.selectAll(".mouse-per-line circle")
+        d3.selectAll(".mouse-per-line2 circle")
           .style("opacity", "1");
-        d3.selectAll(".mouse-per-line text")
+        d3.selectAll(".mouse-per-line2 text")
           .style("opacity", "1");
       })
-      .on('mousemove', function(event: MouseEvent) { // mouse moving over canvas
-        var mouse = d3.pointer(event);
-        d3.select(".mouse-line")
-          .attr("d", function() {
-            var d = "M" + mouse[0] + "," + height;
-            d += " " + mouse[0] + "," + 0;
-            return d;
+      .on('mousemove', function (event: MouseEvent) { // update tooltip content, line, circles and text when mouse moves
+        var mouse = d3.pointer(event)
+
+         d3.selectAll(".mouse-per-line2")
+           .attr("transform", (d, i) => {
+             
+             var xDate = xScale.invert(mouse[0])             
+             var bisect = d3.bisector(d => d[0]).left 
+             var idx = bisect(d["values"], xDate);
+
+             d3.select(".mouse-line2")
+              .attr("d", function () {
+                var data = "M" + xScale(d['values'][idx][0]) + "," + height;
+                data += " " + xScale(d['values'][idx][0]) + "," + 0;
+                return data;
+              });
+            console.log(d);
+
+            // var beginning = 0,
+            //     end = lines[i].getTotalLength(),
+            //     target = null;
+            
+            //     let pos
+            // while (true){
+            //   target = Math.floor((beginning + end) / 2);
+            //   pos = lines[i].getPointAtLength(target);
+            //   if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+            //       break;
+            //   }
+            //   if (pos.x > mouse[0])      end = target;
+            //   else if (pos.x < mouse[0]) beginning = target;
+            //   else break; //position found
+            // }
+            
+            // d3.select(this).select('text')
+            //   .text(y.invert(pos.y).toFixed(2));
+
+            return "translate(" + xScale(d['values'][idx][0]) + "," + y(d['values'][idx][1]) + ")"
           });
-
-        // d3.selectAll(".mouse-per-line")
-        //   .attr("transform", function(d, i) {
-        //     console.log(width/mouse[0])
-        //     var xDate = x.invert(mouse[0]),
-        //         bisect = d3.bisector(function(d) { return d.date; }).right;
-            
-        //     var beginning = 0,
-        //         end = lines[i].getTotalLength(),
-        //         target = null;
-
-        //     while (true){
-        //       target = Math.floor((beginning + end) / 2);
-        //       pos = lines[i].getPointAtLength(target);
-        //       if ((target === end || target === beginning) && pos.x !== mouse[0]) {
-        //           break;
-        //       }
-        //       if (pos.x > mouse[0])      end = target;
-        //       else if (pos.x < mouse[0]) beginning = target;
-        //       else break; //position found
-        //     }
-            
-        //     d3.select(this).select('text')
-        //       .text(y.invert(pos.y).toFixed(2));
-              
-        //     return "translate(" + mouse[0] + "," + pos.y +")";
-        //   });
-      });
+      })
+      
   }
-
 
   async getFolder(): Promise<string> {
     return this.http.get<any>(`assets/data/sp500.json`).toPromise();
